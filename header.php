5807c9854b0a8abaa045f9b4df156196d1b6279b
Jacques Uys <jaxuys@gmail.com>
2016-08-17 20:58:11 +0200

    initial commit

---
+++ header.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The template for displaying the header
+ *
+ * Displays all of the head element and everything up until the "site-content" div.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fifteen
+ * @since Twenty Fifteen 1.0
+ */
+?><!DOCTYPE html>
+<html class="no-js">
+<head>
+	<?php wp_head(); ?>
+</head>
+
+<body>
+<header>
+	This is a header, test!
+</header>
